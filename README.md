6077ffc143133c410ee26da176de500293a6f448
Patrick Kontschak <patrick.kontschak@gmail.com>
2012-08-06 11:33:53 -0400

    Create application
    I designed the user interface and declared a textField outlet as well as actions for the start and stop buttons in the AppDelegate.rb.
    
    My current problem is that I cannot assign the textField outlet to my text field nor the actions to any of my buttons through the Interface Builder.
    
    Signed-off-by: Patrick Kontschak <patrick.kontschak@gmail.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+Simple application using MacRuby
+===
+
+[Based on the tutorial on MacRuby's GitHub repository](https://github.com/MacRuby/MacRuby/wiki/Creating-a-simple-application).
--- StopWatch/AppDelegate.rb
+++ StopWatch/AppDelegate.rb
@@ -8,8 +8,16 @@
 
 class AppDelegate
   attr_accessor :window
+  attr_accessor :textField # Outlet
   def applicationDidFinishLaunching(a_notification)
     # Insert code here to initialize your application
   end
+  
+  def startTimer(sender)
+    # This method is called when the start button is clicked.
+  end
+  
+  def stopTimer(sender)
+    # This method is called when the stop button is clicked.
+  end
 end
-
